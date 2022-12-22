bd3b9fc1e5a0f29e7271d4130b150793eaf035f9
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-12-22 22:38:34 +0545

    Update playerController.tsx

--- src/feature/dashboard/containers/playerController.tsx
+++ src/feature/dashboard/containers/playerController.tsx
@@ -6,7 +6,7 @@ export const PlayerContainer = ({ streamUrl }: { streamUrl: string }) => {
     return (
         <React.Fragment>
             <ReactHlsPlayer
-                src={`http://139.162.230.224/${streamUrl}` as string}
+                src={`http://212.71.235.213/${streamUrl}` as string}
                 autoPlay={true}
                 hlsConfig={{
                     maxLoadingDelay: 4,
